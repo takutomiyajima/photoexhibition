e67d8e4c109ebc9acb6a5d889ae93f4bc9053b3f
takutomiyajima <takutomiyajima3820@gmail.com>
2025-11-18 16:10:14 +0900

    UIの更新

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,21 @@
 import "./globals.css";
 
-
 export const metadata = { title: "冒険 Exhibition" };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
-      <head></head>
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+
+      </head>
       <body className="bg-[#7db3dc] text-white antialiased">{children}</body>
     </html>
   );
